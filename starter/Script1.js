d3f66d7f9b44e699068ad64115e66fe3d87193fc
saivinaySJ <saivinay130@gmail.com>
2023-12-06 14:38:36 +0530

    repetation projects script1 added to new branch

---
+++ starter/Script1.js
@@ -0,0 +1,83 @@
+// 1st step
+/*
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+// 2nd and 3rd step:-
+
+const openModal = function () {
+  //7th step:-
+  //   console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// 4th step:-how to add it again
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+//  5th step:-
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+// 6th step :- Now if you see the 4th and 5th step the code is repeating , we dont want to do that ...please remember the "DON'T REPEAT YOURSELF " principle:
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// 8th step:- please study the explaining part which is in the A4 sheet which you wrote:
+
+// example for keydown :
+document.addEventListener('keydown', function (e) {
+  //   console.log('A key was pressed');
+
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) closeModal();
+  }
+});
+
+// Repetation of Modal window project:-
+
+/*const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (p) {
+  console.log(p.key);
+  if (p.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) closeModal();
+  }
+});*/
